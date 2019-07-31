9e420b5edf6559d23fc1184795b0f03a983b30e8
dmb <dmb@servyou.com.cn>
2019-07-31 13:10:06 +0800

    线程池功能

--- src/main/java/cn/com/fox/ServletInitializer.java
+++ src/main/java/cn/com/fox/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.hz.fox;
+package cn.com.fox;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/test/java/cn/com/fox/FoxApplicationTests.java
+++ src/test/java/cn/com/fox/FoxApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hz.fox;
+package cn.com.fox;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
